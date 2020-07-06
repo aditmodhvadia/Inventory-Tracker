efb9f6bbb012471685dd8c0da0a872a09419dbbc
Adit Modhvadia <adit.modhvadia@gmail.com>
2020-07-05 23:04:26 -0400

    - add login flow

--- app/src/main/java/com/fazemeright/myinventorytracker/firebase/models/Result.kt
+++ app/src/main/java/com/fazemeright/myinventorytracker/firebase/models/Result.kt
@@ -1,5 +1,15 @@
 package com.fazemeright.myinventorytracker.firebase.models
 
-sealed class Result<out R>
-data class Success<out T>(val msg: String = "", val data: T) : Result<T>()
-data class Failure(val msg: String) : Result<Nothing>()
+
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T, val msg: String = "") : Result<T>()
+    data class Error(val exception: Exception? = null, val msg: String) : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data, msg=$msg]"
+            is Error -> "Error[exception=$exception, msg=$msg]"
+        }
+    }
+}
