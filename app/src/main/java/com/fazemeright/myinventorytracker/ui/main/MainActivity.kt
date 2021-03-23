8595c6ee834900865301292557dd308a39948d00
Adit Modhvadia <adit.modhvadia@gmail.com>
2021-03-23 16:12:20 -0400

    - fix build error with missing import

--- app/src/main/java/com/fazemeright/myinventorytracker/ui/main/MainActivity.kt
+++ app/src/main/java/com/fazemeright/myinventorytracker/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.fazemeright.myinventorytracker.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fazemeright.myinventorytracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
