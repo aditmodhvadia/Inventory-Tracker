f498bac6f564a212b8b91325a4e63fc4c51b5174
Adit Modhvadia <adit.modhvadia@gmail.com>
2021-08-15 14:08:58 -0400

    Add compose
    Update compose version

---
+++ app/src/main/java/com/fazemeright/myinventorytracker/ui/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.fazemeright.myinventorytracker.ui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Primary = Color(0XFF1976D2)
+val PrimaryLight = Color(0XFF63A4FF)
+val PrimaryDark = Color(0XFF004BA0)
