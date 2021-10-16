18cc47a9116acd2102a34267106060f027ef42b2
Adit Modhvadia <adit.modhvadia@gmail.com>
2021-10-15 21:41:56 -0500

    Initial commit

---
+++ ComposeToDo/app/src/main/java/com/example/composetodo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.composetodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composetodo.ui.theme.ComposeToDoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeToDoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeToDoTheme {
+        Greeting("Android")
+    }
+}
